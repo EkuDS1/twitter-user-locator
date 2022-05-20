01aa66f184db1ac9117a6700c7cc77427dc03cd9
Vin-Xi <hamdanrashid4@hotmail.com>
2022-05-20 13:45:49 +0500

    Response catered in front end

--- interface/my-app/src/Home.jsx
+++ interface/my-app/src/Home.jsx
@@ -5,24 +5,37 @@ import React,{ useEffect,useState } from 'react';
 const Home = () => {
   
 	const [getMessage,setMessage] = useState({})
-
+	const [username,setUsername] = useState('')
+	const [getCoordinates,setCoordinates] = useState({})
+	// const [getJSON, setJSON] = useState({})
 	useEffect(()=>{
-		axios.get('http://localhost:5000/Lord_Arse').then(response=>{
-			console.log("Success",response)
-			setMessage(response)
+		console.log(getMessage.location)
+		console.log(getCoordinates.lat)
+		console.log(getCoordinates.long)	
+		console.log(`Form Submitted,${username}`)
+	},[getMessage]);
+	
+	const handleSubmit = (e) =>{
+		axios.get(`http://localhost:5000/${username}`).then(response=>{
+			console.log("Success",response.data)
+			setMessage(response.data)
+			setCoordinates({"lat":response.data.lat[0],"long":response.data.long[0]})
 		}).catch(error =>{console.log(error)})
-	})
+		e.preventDefault();
+		
+	}
 
   return (
-    <div>
-		<form>
-			<label>
+    <div className="wrapper">
+		<form onSubmit = {handleSubmit}>
+			<label >
 				Username:	
-				<input type="text" name="username"/>
+				<input className="searchBox" type="text" onChange={(e)=>setUsername(e.target.value)} name="username"/>			
 			</label>
+			<button type="submit">Locate!</button>
 		</form>
 	</div>
-
+	
     
   );
 }
