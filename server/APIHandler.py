563865994b26ea794d6302c8d0480eb595ebfd0b
Vin-Xi <hamdanrashid4@hotmail.com>
2022-05-22 20:29:24 +0500

    fixed model errors

--- server/APIHandler.py
+++ server/APIHandler.py
@@ -17,7 +17,10 @@ class APIHandler(Resource):
         # d=c[0].tolist()                               #   CONVERT TO LIST FOR SERIALIZATION
         # geoxml=reverseGeocode(d[0],d[1])              #   CONVERT TO COUNTRY
         coord=forwardGeocode(loc)
-        
+        print(coord[0],coord[1],type(coord[0]),type(coord[1]))
+        if (coord[0]>float(60.0) or (coord[0]<float(0.0))) and (coord[1]>float(-50.0)) or (coord[1] < float(-100.0)):
+            print(coord)
+            coord[0]=40.7128;coord[1]=74.0060
         concat={"lat":coord[0],"long":coord[1],"location":loc}
         return concat 
 
